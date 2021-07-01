0abde45cfa96da05aa1fcecefd97d79ea653fa21
August <auggierutter@gmail.com>
2021-07-01 17:43:50 -0400

    commit tree cba09214439adc212ad8fa2c02620dc636ef649d

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 let addToy = false;
-
+const newCard= document.createElement('div')
+const addBtn = document.querySelector("#new-toy-btn");
+const toyFormContainer = document.querySelector(".container");
+const get= document.getElementById('toy-collection')
+const container = []
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
+  fetch('http://localhost:3000/toys',)
+  .then((response)=>response.json())
+  .then((data)=> get.innerHTML+=`
+  <div class='card' id='${data[0].id}'>
+  <h2>${data[0].name}</h2>
+  <img src='${data[0].image}>'
+  </div>`)
+  
+
   addBtn.addEventListener("click", () => {
+    
+
+ 
+    
     // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
